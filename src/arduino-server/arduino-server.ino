6722f9fb7b4568579a2868fe988db1b7fa5a09b6
Gabriel Lecomte <gabriel1906@gmail.com>
2017-06-21 13:51:07 -0300

    Creates Arduino Server file with proper header

---
+++ src/arduino-server/arduino-server.ino
@@ -0,0 +1,26 @@
+/*
+  Project Name: Simple Arduino Communication Project
+  Authors:
+      * Gabriel Lecomte Pinho e Souza
+      * Pedro Vidal Sales
+  Version: 1.0.0
+  Boards Supported:
+    * Arduino *
+    * NodeMCU
+  Dependencies:
+    * Ethernet, 1.1.2
+  Description:
+    Descrição do projeto ...........................................
+  Continuação da descrição do projeto.
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
