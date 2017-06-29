82435a2391abf3050c19cd6e3b5af63ddd01bec0
Gabriel Lecomte <gabriel1906@gmail.com>
2017-06-28 22:36:05 -0300

    Implements message input and sending and quitting mechanism

--- src/client/client.py
+++ src/client/client.py
@@ -5,4 +5,14 @@ PORTA = 7070
 
 client = socket.socket()
 client.connect((SERVIDOR, PORTA))
+print 'Conexão iniciada! (Para encerrar digite Q no campo de mensagem)'
+while 1:
+	message = raw_input()
+	finalMessage = ''
+	finalMessage.join(message.split(' '))
+	if (message[0] == 'Q')
+		break
+	client.sendall(finalMessage)
+	print 'Mensagem enviada: ', finalMessage
+
 client.close()
