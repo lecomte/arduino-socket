f61985e49b67afa63a46eea227f9e6d9b2d48690
Gabriel Lecomte <gabriel1906@gmail.com>
2017-06-28 21:31:20 -0300

    Joins GET and SET operations and implements MSG operation

--- src/server/server.py
+++ src/server/server.py
@@ -8,7 +8,7 @@ PORTA = 7070
 ARDUINO_SERVIDOR = '192.168.1.44'
 ARDUINO_PORTA  = 8080
 
-def get(envia):
+def getSet(envia):
 	arduinoSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	destino = (ARDUINO_SERVIDOR, ARDUINO_PORTA)
 	arduinoSocket.connect(destino)
@@ -24,9 +24,9 @@ def conectado(conex, cliente):
     while True:
         s = conex.recv(1024)
         mensagem = ctypes.create_string_buffer(s)
-        if (mensagem[0] == 'G'):
+        if (mensagem[0] == 'G' or mensagem[0] == 'S'):
         	print 'Operacao GET recebida de ', cliente
-        	ans = get(mensagem)
+        	ans = getSet(mensagem)
         	if (ans[0] == 'R'):
         		print 'Valor recebido de Arduino (', ARDUINO_SERVIDOR, ':' , ARDUINO_PORTA, ') -> ', ans[1:]
         		conex.sendall(ans)
@@ -35,7 +35,8 @@ def conectado(conex, cliente):
         		print 'Erro recebido de Arduino (', ARDUINO_SERVIDOR, ':' , ARDUINO_PORTA, ')'
         		conex.sendall(ans)
         		print 'Erro transmitido ao cliente ', cliente
-        print cliente, ' Mensagem recebida: ', mensagem
+        elif (mensagem[0] == 'M')
+            print cliente, ' Mensagem recebida: ', mensagem[1:]
     print 'Encerrando conexao: ', cliente
     conex.close()
     thread.exit()
