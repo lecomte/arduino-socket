b3d8d5fb6411b9b62d272c8b6c971851de698d8b
Gabriel Lecomte <gabriel1906@gmail.com>
2017-06-28 21:42:52 -0300

    Adds MAC and IP constants

--- src/arduino-server/arduino-server.ino
+++ src/arduino-server/arduino-server.ino
@@ -17,6 +17,9 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192, 168, 1, 155);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
