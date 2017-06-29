603d39cb4ef2286ccd5d560641867f3717da81e5
Gabriel Lecomte <gabriel1906@gmail.com>
2017-06-28 22:57:53 -0300

    Corrects syntax and implementation errors

--- src/client/client.py
+++ src/client/client.py
@@ -8,13 +8,15 @@ client.connect((SERVIDOR, PORTA))
 print 'Conexao iniciada! (Para encerrar digite Q no campo de mensagem)'
 while 1:
 	message = raw_input()
-	finalMessage = ''
-	finalMessage.join(message.split(' '))
 	if (message[0] == 'Q') :
+		client.sendall(message)
 		break
+	finalMessage = message[0] + message[2:]
+	print finalMessage
 	client.sendall(finalMessage)
 	print 'Mensagem enviada: ', finalMessage
-	answer = client.recv(1024)
-	print 'Resposta recebida: ', answer[1:]
+	if (finalMessage[0] != 'M') :
+		answer = client.recv(1024)
+		print 'Resposta recebida: ', answer[1:]
 
 client.close()
