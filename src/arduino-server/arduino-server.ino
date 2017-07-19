51a705fe35bebb921ca937c06ce790426384263f
Gabriel Lecomte <gabriel1906@gmail.com>
2017-07-19 15:58:22 -0300

    Send/receive messages via Serial!

--- src/arduino-server/arduino-server.ino
+++ src/arduino-server/arduino-server.ino
@@ -1,3 +1,4 @@
+
 /*
   Project Name: Simple Arduino Communication Project
   Authors:
@@ -14,7 +15,6 @@
     Um simples robô que recebe solicitações dos tipos Get e Set para modificar ou consultar valores de pinos do Arduino ou de um vetor de inteiros.
 */
 #include <SPI.h>
-#include <Ethernet.h>
 
 #define ENCODER_DIREITA 2
 #define ENCODER_ESQUERDA 3
@@ -58,20 +58,11 @@ volatile int contador_esquerda = 0;
 int velocidadeDireita  = 230;
 int velocidadeEsquerda = 230;
 
-
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(192, 168, 141, 30);
 int values[27];
-
-EthernetServer server(8080);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Ethernet.begin(mac, ip);
-  server.begin();
-  Serial.print("Servidor iniciado em: ");
-  Serial.println(Ethernet.localIP());
 
   // Configuração dos pinos da Ponte H
   pinMode(DIRECAO_DIREITA_1, OUTPUT);
@@ -90,66 +81,43 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("Conexao recebida!");
-    String message = "";
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        if (c != '\n') {
-          message += c;
-        }
-        else {
+  if (Serial.available()) {
+        String s = Serial.readString();
           if (message[0] == 'G') {
-            Serial.println("Operacao GET recebida!");
             int result, pos = message.substring(1, message.length()).toInt();
             if (pos <= 13) {
               result = digitalRead(pos);
             }
             else if (pos == 40) {
-              result = contador_direita;  
+              result = (contador_direita + contador_esquerda) / 2;  
             }
             else {
               result = values[pos - 13];
             }
-            Serial.print("Valor solicitado ");
-            Serial.print(pos);
-            Serial.print(": ");
-            Serial.println(result);
             String sres(result);
-            client.print("R" + sres);
+            Serial.println("R" + sres);
           }
           else if (message[0] == 'S') {
-            Serial.println("Operacao SET recebida!");
             message = message.substring(1, message.length());
             int eqPos = message.indexOf('=');
             int dest = message.substring(0, eqPos).toInt(), num = message.substring(eqPos + 1, message.length()).toInt();
-            Serial.print("Valor solicitado ");
-            Serial.print(dest);
-            Serial.print(": ");
-            Serial.println(num);
             if (dest <= 13) {
               if (num > 1 || num < 0) {
-                client.println("R0");
+                Serial.println("R0");
               }
               else {
                 digitalWrite(dest, num);
-                client.println("R1");
+                Serial.println("R1");
               }
             }
             else {
               values[dest - 13] = num;
-              client.println("R1");
+              Serial.println("R1");
             }
           }
           message = "";
           break;
-        }
-      }
     }
-    client.stop();
-    Serial.println("Conexao encerrada!");
   }
   else {
     if (values[1] >= 1) {
